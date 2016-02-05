e44c1f319a2ce604b9b297d16ff343171f4cab32
antpachon <antpachon@gmail.com>
2016-02-05 23:02:38 +0100

    Set Firebase reference from main project

--- app/src/main/java/com/suitapps/reglogsample/SampleApplication.java
+++ app/src/main/java/com/suitapps/reglogsample/SampleApplication.java
@@ -5,11 +5,14 @@ import android.content.Context;
 
 import com.firebase.client.Firebase;
 import com.parse.Parse;
+import com.suitapps.reglog.RegLog;
 
 /**
  * Created by antonio on 22/01/2016.
  */
 public class SampleApplication extends Application {
+
+    Firebase mFireBaseRef;
 
     @Override
     public void onCreate() {
@@ -22,6 +25,10 @@ public class SampleApplication extends Application {
 
         //If Using Firebase.
         Firebase.setAndroidContext(this);
+        //General Firebase reference used allong all your app
+        mFireBaseRef = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/");
+        //Use that same reference to perform login and register with Reglog.
+        RegLog.setFirebaseReference(mFireBaseRef);
     }
 
 
